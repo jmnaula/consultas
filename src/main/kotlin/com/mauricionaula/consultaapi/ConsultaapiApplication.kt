680648f36a9a33d7812fa03c3cd8c0915e972391
666Mauri <jmnaula@sudamericano.edu.ec>
2021-11-22 19:34:34 -0500

    first commit

---
+++ src/main/kotlin/com/mauricionaula/consultaapi/ConsultaapiApplication.kt
@@ -0,0 +1,11 @@
+package com.mauricionaula.consultaapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConsultaapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConsultaapiApplication>(*args)
+}
---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS  paciente(
+    id cedula,
+    edad VARCHAR(45) NOT NULL,
+    peso VARCHAR(45) NULL,
+    PRIMARY KEY (id)
+    );
